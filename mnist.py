40e343f45967297fe7addde6d1b0c753581706c2
Ryan <ryankluzinski@gmail.com>
2018-04-06 15:54:09 -0600

    batch gradient descent implemented

--- mnist.py
+++ mnist.py
@@ -1,50 +1,106 @@
 """
 Written by Ryan Kluzinski
-Last Edited March 30, 2018
+Last Edited April 6, 2018
 
 Functions for loading the MNIST data.
+
+TODO:
+-Handle wrong filenames?
 """
 
 import numpy as np
 
 def read_int(fp):
     """
-    Reads an integer from a file open in binary mode.
-
-    TODO: args, returns
+    Reads an integer from a file open in read binary mode.
     """
     return int.from_bytes(fp.read(4), byteorder='big')
 
-def loadLabels(filename):
+
+def load_labels(filename):
     """
-    Reads labels from the MNIST data.
-    
-    TODO: args, returns
+    Reads labels from the given MNIST file. Returns a list of
+    one-hot vectors.
     """
     
     with open(filename, 'rb') as infile:
+        # read magic number and count
         magic = read_int(infile)
         count = read_int(infile)
+        
+        # read entire file into a buffer
+        npbuffer = np.frombuffer(infile.read(count), dtype=np.uint8)
 
-        labels = np.frombuffer(infile.read(count), dtype=np.uint8)
+    # creates numpy array of onehot vectors
+    labels = np.zeros([count, 10])
+    labels[np.arange(count), npbuffer] = 1 
 
     return labels
 
 
-def loadVectors(filename):
+def load_vectors(filename):
     """
-    Reads vectors from the MNIST data.
-
-    TODO: args, returns
+    Reads input data from the given MNIST file. Returns a list of
+    vectors containing the input data.
     """
 
     with open(filename, 'rb') as infile:
+        # read magic number and count
         magic = read_int(infile)
         count = read_int(infile)
 
+        # read num of rows and columns of data
         rows = read_int(infile)
         columns = read_int(infile)
 
-        vectors = np.frombuffer(infile.read(count*rows*columns), dtype=np.uint8)
+        # number of bytes to read
+        bytecount = count*rows*columns
 
-    return vectors.reshape(count, rows*columns) / 255
+        # read entire file into a buffer
+        npbuffer = np.frombuffer(infile.read(bytecount),
+                                 dtype=np.uint8)
+
+    # creates array of scaled input vectors
+    vectors = npbuffer.reshape(count, rows*columns) / 255
+    
+    return vectors
+
+
+def load_training():
+    labels = load_labels("mnist/train-labels.idx1-ubyte")
+    vectors = load_vectors("mnist/train-images.idx3-ubyte")
+
+    return labels, vectors
+
+
+def load_testing():
+    labels = load_labels("mnist/t10k-labels.idx1-ubyte")
+    vectors = load_vectors("mnist/t10k-images.idx3-ubyte")
+
+    return labels, vectors
+
+
+def load_mnist():
+    return load_training() + load_testing()
+
+
+# for testing
+def main():
+    training_labels, training_vectors, testing_labels,\
+    testing_vectors = load_mnist()
+
+    print("Training Labels:")
+    print(training_labels.shape)
+
+    print("\nTraining Images:")
+    print(training_vectors.shape)
+
+    print("\nTesting Labels:")
+    print(testing_labels.shape)
+
+    print("\nTesting Images:")
+    print(testing_vectors.shape)
+
+
+if __name__ == "__main__":
+    main()
--- neuralnet.py
+++ neuralnet.py
@@ -1,6 +1,6 @@
 """
 Written by Ryan Kluzinski
-Last Edited March 30, 2018
+Last Edited April 6, 2018
 
 A neural network class.
 """
@@ -9,61 +9,147 @@ import numpy as np
 
 class NeuralNetwork:
     """
-    A single-layer neural network class.
+    A class for 3-layer neural network.
     """
 
-    def __init__(self, num_input, num_hidden, num_output):
+    def __init__(self, sizes):
         """
         Initializes the network with random weights.
-
-        TODO: args, returns
         """
 
-        self.num_input = num_input
-        self.num_hidden = num_hidden
-        self.num_output = num_output
+        # only 3 layer-networks are supported
+        assert len(sizes) == 3
 
-        # weight matrices
-        self.weights_1 = 2 * np.random.rand(num_hidden, num_input) - 1
-        self.weights_2 = 2 * np.random.rand(num_output, num_hidden) - 1
+        # stores the layer sizes
+        self.sizes = sizes
 
-        # bias vectors
-        self.bias_1 = np.random.rand(num_hidden, 1)
-        self.bias_2 = np.random.rand(num_output, 1)
+        # initializes the weight matrices
+        self.W1 = 2 * np.random.rand(sizes[1], sizes[0]) - 1
+        self.W2 = 2 * np.random.rand(sizes[2], sizes[1]) - 1
 
-    def _sigmoid(self, x, deriv=False):
+        # initializes the bias vectors
+        self.b1 = 2 * np.random.rand(sizes[1], 1) - 1
+        self.b2 = 2 * np.random.rand(sizes[2], 1) - 1
+
+
+    def sigmoid(self, x, deriv=False):
         """
-        The sigmoid function and it's derivative.
-
-        TODO: args, returns
+        Uses the sigmoid function for the nonlinear activation
+        function. Returns the derivative if deriv=True.
         """
 
+        # return derivative
         if deriv:
-            temp = self._sigmoid(x)
-            return temp * (1 - temp)
+            s = self.sigmoid(x)
+            return s*(1-s)
 
-        return 1/(1 + np.exp(-x))
+        return 1/(1+np.exp(-x))
+
 
     def feedforward(self, vector):
         """
-        Feeds a vector in to the neural network a return another vector.
-
-        TODO: args, returns
+        Feeds a column vector into the neural network and returns 
+        the ouput vector.
         """
 
-        assert vector.shape == (self.num_input, 1)
+        # input must be a column vector
+        assert vector.shape == (self.sizes[0], 1)
 
-        # stores the most recent activation fore backprop
-        self.activation_1 = self._sigmoid( self.weights_1.dot(vector) + self.bias_1 )
-        self.activation_2 = self._sigmoid( self.weights_2.dot(self.activation_1) + self.bias_2 )
+        z1 = self.W1.dot(vector) + self.b1
+        a1 = self.sigmoid(z1)
 
-        return self.activation_2
+        z2 = self.W2.dot(a1) + self.b2
+        a2 = self.sigmoid(z2)
 
-    def backpropogate(self, vector):
+        return a2
+
+
+    def backpropagate(self, X, Y):
         """
-        Uses backpropogation to find the gradient with respect to one training example.
-
-        TODO: args, return
+        Locally computes the gradient of the cost function.
         """
-        
-        pass
+
+        assert len(X) == len(Y)
+        batch_size = len(X)
+
+        # stores gradients of the weights and biases
+        dW1 = np.zeros([self.sizes[1], self.sizes[0]])
+        dW2 = np.zeros([self.sizes[2], self.sizes[1]])
+        db1 = np.zeros([self.sizes[1], 1])
+        db2 = np.zeros([self.sizes[2], 1])
+
+        loss = 0
+
+        for x, y in zip(X, Y):
+            # convert to column vectors
+            x = x.reshape(self.sizes[0], 1)
+            y = y.reshape(self.sizes[2], 1)
+
+            # computes the forward propogation
+            z1 = self.W1.dot(x) + self.b1
+            a1 = self.sigmoid(z1)
+            z2 = self.W2.dot(a1) + self.b2
+            a2 = self.sigmoid(z2)
+
+            # computes error and loss
+            error = a2 - y
+            loss += np.sum(np.square(error))
+
+            # computes the backwards propagating errors
+            delta2 = np.multiply(error,
+                                 self.sigmoid(z2, deriv=True))
+            delta1 = np.multiply(self.W2.T.dot(delta2),
+                                 self.sigmoid(z1, deriv=True))
+
+            # updates the gradients
+            dW1 += delta1.dot(x.T)
+            dW2 += delta2.dot(a1.T)
+            db1 += delta1
+            db2 += delta2
+
+        #average of individual gradients
+        dW1 = dW1 / batch_size
+        dW2 = dW2 / batch_size
+        db1 = db1 / batch_size
+        db2 = db2 / batch_size
+
+        return loss, dW1, dW2, db1, db2
+
+
+    def batch_grad_descent(self, X, Y, alpha, epochs):
+        """
+        Uses the entire set of training data to compute the
+        gradient for each epoch. The weights and biases are only
+        updated once per epoch.
+        """
+
+        for i in range(epochs):
+            loss, dW1, dW2, db1, db2 = self.backpropagate(X, Y)
+
+            # update parameters
+            self.W1 -= alpha * dW1
+            self.W2 -= alpha * dW2
+            self.b1 -= alpha * db1
+            self.b2 -= alpha * db2
+            
+            print(loss)
+
+
+def main():
+    # test data
+    X = np.array([[1,0,1], [0,0,1], [1,0,0]])
+    Y = np.array([[1,0], [0,1], [1,1]])
+
+    # initialize and train
+    testNN = NeuralNetwork([3,2,2])
+    testNN.batch_grad_descent(X, Y, 1.0, 10000)
+
+    # print results
+    for x in X:
+        print(testNN.feedforward(x.reshape(3,1)))
+
+
+if __name__ == "__main__":
+    main()
+
+                
